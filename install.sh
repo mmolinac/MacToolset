0a92b74e3ec745295afd70c56d7b2e1c46069246
Manuel Molina Cuberos <manuelmc@whisky.local>
2019-04-17 18:50:56 +0200

    install.sh: Now using `brew bundle` instead of `brew install` or `brew cask install`.

--- install.sh
+++ install.sh
@@ -2,12 +2,28 @@
 # This script installs Home Brew and then a list of predefined tools
 # that suit me specially well.
 
+# Functions
+
 # Learnt in https://apple.stackexchange.com/questions/98080/can-a-macs-model-year-be-determined-with-a-terminal-command
-hwmodel=`curl -s https://support-sp.apple.com/sp/product?cc=$(
-  system_profiler SPHardwareDataType \
-    | awk '/Serial/ {print $4}' \
-    | cut -c 9-
-) | sed 's|.*<configCode>\(.*\)</configCode>.*|\1|'`
+
+hardwareModel() {
+    local var=`curl -s https://support-sp.apple.com/sp/product?cc=$(
+      system_profiler SPHardwareDataType \
+      | awk '/Serial/ {print $4}' \
+      | cut -c 9-
+      ) | sed 's|.*<configCode>\(.*\)</configCode>.*|\1|'`
+    echo "$var"
+}
+
+brewBundle() {
+    echo "* Installing $2 ..."
+    brew bundle install --file=./${1}.Brewfile|grep -v "^Using homebrew"|grep -v ^"Homebrew Bundle complete"
+    echo " "
+}
+
+# Main block
+
+hwmodel=$(hardwareModel)
 echo "* Checking hardware model: $hwmodel"
 echo " "
 echo "* Checking for Xcode command line developer tools ..."
@@ -22,64 +38,58 @@ echo " "
 brew --version || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo "* Updating package database ..."
 brew update
-echo "* Installing some editors ..."
-brew cask install visual-studio-code
-echo "* Installing a benchmarking tool ..."
-brew cask install geekbench
-echo "* Installing web browsers"
-brew cask install firefox google-chrome
-echo "* Installing chat, email and collaboration tools ..."
-brew cask install skype zoomus thunderbird
-echo "* Installing THE office suite ..."
-brew cask install libreoffice libreoffice-language-pack
-echo "* Installing Hashicorp tools ..."
-brew cask install vagrant vagrant-manager
-echo "* Installing automation tools ..."
+
+# We'll provide a line per section to install:
+#   brewBundle filename "simple description"
+# where 'filename' references a file in the repo called 'filename.Brewfile'
+# with the description inside
+
+brewBundle editors "some editors"
+brewBundle benchmark "benchmarking tools"
+brewBundle browsers "web browsers"
+brewBundle chatcollab "chat, email and collaboration tools"
+brewBundle office "THE office suite"
+brewBundle hashicorp "Hashicorp tools"
+
+# Permissions needed in order to install Ansible
 if [ `stat -f %Su /usr/local/lib/pkgconfig` != `whoami` ]; then
   echo " The following directories are not writable by your user: /usr/local/lib/pkgconfig"
   echo "* Changing permissions ..."
   # We change permissions of that folder
   sudo chown -R $(whoami) /usr/local/lib/pkgconfig
+  echo " "
 fi
-brew install ansible ansible-lint
-echo "* Installing virtualization tools"
-brew cask install virtualbox virtualbox-extension-pack
-echo "* Installing cloud tools"
-brew cask install google-cloud-sdk
-echo "* Installing communication and terminal tools ..."
-brew cask install coccinellida cyberduck cord zterm
-echo "* Installing some amusements ..."
-brew cask install spotify spotifree vlc disk-inventory-x
-echo "* Installing API tools ..."
-brew cask install postman
-echo "* Installing GitHub tools ..."
-brew cask install github
-echo "* Installing compression tools ..."
-brew cask install rar
-echo "* Installing database frontends ..."
-brew cask install mysqlworkbench
-echo "* Installing X frontend for Mac, required by other tools ..."
-brew cask install xquartz
-echo "* Installing design and photo tools ..."
-brew cask install gimp inkscape
-echo "* Installing Java ..."
-brew cask install java
+brewBundle automation "automation tools"
+# Ansible needs this authentication module for Google Cloud Platform:
+/usr/local/Cellar/ansible/*/libexec/bin/pip3 install google-auth && echo " "
+
+brewBundle virt "virtualization tools"
+brewBundle cloud "cloud tools"
+# PIP module needed for SoftLayer, not available in Brew
+pip3 install -U SoftLayer && echo " "
+
+brewBundle comm "communications and terminal tools"
+brewBundle amuse "some amusements"
+brewBundle api "API tools"
+brewBundle git "git tools"
+brewBundle compress "compression tools"
+brewBundle database "database tools"
+brewBundle design "image design and photo tools"
+brewBundle java "Java SDK"
+brewBundle mediamgmt "CD/DVD recording and hard drive management software"
+
 if echo $hwmodel|grep -i book > /dev/null ; then
-  echo "* Installing battery monitor ..."
-  brew install coconutbattery
+  brewBundle battery "battery monitor"
 fi
+
 # Docker
 if [ `sysctl kern.hv_support|awk '{print $2}'` -ne 1 ]; then
-  echo "* Installing docker-toolbox ..."
-  #brew cask install docker-toolbox
+  brewBundle dockertool "docker-toolbox"
 else
-  echo "* Installing docker, as it's supported with this hardware ..."
-  brew cask install docker
+  brewBundle docker "docker, as it's supported with this hardware"
 fi
-echo "* Installing mas utility to allow command line management of App Store installed items ..."
+
+echo "* Upgrading App Store applications ..."
 # Read on https://www.podfeet.com/blog/2018/01/reinstall-mac-app-store-apps-from-the-command-line/
-brew install mas
-echo "* Installing Slack from App Store ..."
-mas install 803453959 # Slack 3.3.8
 mas upgrade # Install later versions
 brew cleanup
